d8a808f939820b01e41bc4838e39b4b92ea7e410
salonidayal <66234920+salonidayal@users.noreply.github.com>
2022-05-14 00:05:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const express = require('express');
 
 const app = express();
 
-const url = "https://www.theguardian.com/uk";
+const url = "your website url";
 axios(url)
 .then(response =>{
     const html = response.data
     const $ = cheerio.load(html)
 
     const articles = []
-    $('.fc-item__title',html).each(function(){
+    $('.class name of what you want to search',html).each(function(){
         const title = $(this).text()
         const url = $(this).find('a').attr('href')
         articles.push({ title,url})
